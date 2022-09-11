9d7c55588a70ca7545cd42bd880971b744d6ca2e
Helgard Ferreira <hrfer3@gmail.com>
2022-09-11 13:33:47 +0200

    feat: add central observable event bus

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,6 +1,15 @@
 import { EventObject, Interpreter } from "xstate";
 
+export * from "./symbols";
+
 export type SimpleInterpreter<
   TContext,
   TEvent extends EventObject
 > = Interpreter<TContext, any, TEvent, any, any>;
+
+export interface IEvent<T> {
+  type: string;
+  data: T;
+}
+
+export type EventLike<T = any> = Event | IEvent<T>;
